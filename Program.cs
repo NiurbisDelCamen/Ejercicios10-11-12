ddd2f717bbad7ce60d5c89b64859d7845488ebcf
Niurbis Del Carmen Reyes Jaquez <niurbis_reyes@ucne.edu.do>
2020-02-01 16:22:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea3_EjerciciosConsola.Capitulo10
+{
+    class Ejercicio1
+    {
+        public Ejercicio1()
+        {
+            nombreProducto = "";
+            marcaProducto = "";
+            precio = 0;
+            existencia = 0;
+        }
+        public void Accion()
+        {
+            Console.WriteLine("Ingrese nombre");
+            nombreProducto = Console.ReadLine();
+
+            Console.WriteLine("Ingrese marca producto");
+            marcaProducto = Console.ReadLine();
+
+            Console.WriteLine("Ingrese precio");
+            precio = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Ingrese existencia");
+            precio = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        private string nombreProducto;
+        private string marcaProducto;
+        private decimal precio;
+        private int existencia;
+    }
+  
+}
+
+
+
